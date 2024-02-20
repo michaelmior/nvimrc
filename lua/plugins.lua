30686d80a36040565cda4d11c4e3f7ad0ae7be4a
Michael Mior <mmior@mail.rit.edu>
2024-02-20 11:47:22 -0500

    Add gitsigns

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -15,6 +15,7 @@ require("lazy").setup({
   "nvim-treesitter/nvim-treesitter-context",
   "stevearc/dressing.nvim",
   "farmergreg/vim-lastplace",
+  {"lewis6991/gitsigns.nvim", opts = {}},
   {"Exafunction/codeium.vim", event = "BufEnter"},
   {
     "akinsho/bufferline.nvim",
