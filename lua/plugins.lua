c0041dc14831bc984555c07996d0fdfd7021ea8b
Michael Mior <michael@onechronos.com>
2026-04-06 10:10:10 -0400

    Add bindings for moving between git changes

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -17,7 +17,37 @@ require("lazy").setup({
   "nvim-treesitter/nvim-treesitter-context",
   "lbrayner/vim-rzip",
   "farmergreg/vim-lastplace",
-  {"lewis6991/gitsigns.nvim", opts = {}},
+  {
+    "lewis6991/gitsigns.nvim",
+    opts = {
+      on_attach = function(bufnr)
+        local gitsigns = require('gitsigns')
+
+        local function map(mode, l, r, opts)
+          opts = opts or {}
+          opts.buffer = bufnr
+          vim.keymap.set(mode, l, r, opts)
+        end
+
+        -- Navigation
+        map('n', ']c', function()
+          if vim.wo.diff then
+            vim.cmd.normal({']c', bang = true})
+          else
+            gitsigns.nav_hunk('next')
+          end
+        end, { desc = "Next Git Hunk" })
+
+        map('n', '[c', function()
+          if vim.wo.diff then
+            vim.cmd.normal({'[c', bang = true})
+          else
+            gitsigns.nav_hunk('prev')
+          end
+        end, { desc = "Previous Git Hunk" })
+      end
+    },
+  },
   {"windwp/nvim-autopairs", event = "InsertEnter", opts = {}},
   "darfink/vim-plist",
   {
